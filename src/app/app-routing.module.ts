a864f82b7726010474373d190e76cce6c6623d5a
kevin lugaziva <kevin.lugaziwa@financeplan.biz>
2019-10-04 16:42:17 +0300

    HM-84 fix merge conflict

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,7 +12,6 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent},
   { path: 'maps', loadChildren: () => import('./maps/maps.module').then(m => m.MapsModule) },
   { path: 'orders', loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule) },
-  { path: '**', component: PagenotfoundComponent },
   { path: 'endpoint', loadChildren: () => import('./endpoint/endpoint.module').then(m => m.EndpointModule) },
   { path: 'incidents', loadChildren: () => import('./incidents/incidents.module').then(m => m.IncidentsModule) },
   { path: '**', component: PagenotfoundComponent },
