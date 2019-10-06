b200fd4235d2b07f06928a61c3f60086eb585315
kevin lugaziva <kevin.lugaziwa@financeplan.biz>
2019-10-06 14:21:41 +0300

    HM-86 | add endpoint service

--- src/app/endpoint/endpoint-view/endpoint-view.component.ts
+++ src/app/endpoint/endpoint-view/endpoint-view.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { MdbTablePaginationComponent, MdbTableDirective } from 'angular-bootstrap-md';
+import { Component, OnInit, ViewChild, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import {GetEndpointsService} from './get-endpoints.service'
  
 @Component({
   selector: 'app-endpoint-view',
@@ -6,18 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./endpoint-view.component.scss']
 })
 export class EndpointViewComponent implements OnInit {
+  @ViewChild(MdbTablePaginationComponent, { static: true }) mdbTablePagination: MdbTablePaginationComponent;
+  @ViewChild(MdbTableDirective, { static: true }) mdbTable: MdbTableDirective;
+  elements: any[];
+  previous: any = [];
 
-  elements: any = [
-    {id: 'endpoint 1', first: 'Mark', last: 'Otto'},
-    {id: 'endpoint 2', first: 'Jacob', last: 'Thornton'},
-    {id: 'endpoint 3', first: 'Larry', last: 'the Bird'},
-  ];
 
   headElements = ['Endpoint', 'Date Created', 'Action'];
 
-  constructor() { }
+  constructor(
+    private getEndpoints: GetEndpointsService,
+    private cdRef: ChangeDetectorRef
+    ) {}
 
   ngOnInit() {
+    this.elements = this.getEndpoints.getEndpoint();
+    this.mdbTable.setDataSource(this.elements);
+    this.elements = this.mdbTable.getDataSource();
+    this.previous = this.mdbTable.getDataSource();
   }
 
+  ngAfterViewInit() {
+    this.mdbTablePagination.setMaxVisibleItemsNumberTo(5);
+    this.mdbTablePagination.calculateFirstItemIndex();
+    this.mdbTablePagination.calculateLastItemIndex();
+    this.cdRef.detectChanges();
+  }
+
+  searchItems(search: string) {
+    const prev = this.mdbTable.getDataSource();
+
+    if (!search) {
+      this.mdbTable.setDataSource(this.previous);
+      this.elements = this.mdbTable.getDataSource();
+    }
+
+    if (search) {
+      this.elements = this.mdbTable.searchLocalDataBy(search);
+      this.mdbTable.setDataSource(prev);
+    }
+  }
 }
