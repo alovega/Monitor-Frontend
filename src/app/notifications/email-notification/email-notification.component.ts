6b9d6b902afb14590e80720aadc32ecbd374afd7
kevin lugaziva <kevin.lugaziwa@financeplan.biz>
2019-10-07 11:46:35 +0300

    update email-notification

--- src/app/notifications/email-notification/email-notification.component.ts
+++ src/app/notifications/email-notification/email-notification.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { MdbTablePaginationComponent, MdbTableDirective } from 'angular-bootstrap-md';
+import { Component, OnInit, ViewChild, AfterViewInit, ChangeDetectorRef } from '@angular/core';
+import {GetNotificationsService} from '../get-notifications.service'
 
 @Component({
   selector: 'app-email-notification',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./email-notification.component.scss']
 })
 export class EmailNotificationComponent implements OnInit {
+  @ViewChild(MdbTablePaginationComponent, { static: true }) mdbTablePagination: MdbTablePaginationComponent;
+  @ViewChild(MdbTableDirective, { static: true }) mdbTable: MdbTableDirective;
+  firstItemIndex: any;
+  lastItemIndex: any;
+  elements: any[];
+  previous: any = [];
+  headElements: string[] = [ 'email', 'date-created', 'state'];
 
-  constructor() { }
+  constructor(private getNotifications: GetNotificationsService, private cdRef: ChangeDetectorRef) { }
 
   ngOnInit() {
+    this.elements = this.getNotifications.getEmail()
+    this.mdbTable.setDataSource(this.elements);
+    this.elements = this.mdbTable.getDataSource();
+    this.previous = this.mdbTable.getDataSource();
   }
 
+  ngAfterViewInit() {
+    this.mdbTablePagination.setMaxVisibleItemsNumberTo(5);
+    this.mdbTablePagination.calculateFirstItemIndex();
+    this.mdbTablePagination.calculateLastItemIndex();
+    this.cdRef.detectChanges();
+  }
+
+  searchItems(search: string) {
+    const prev = this.mdbTable.getDataSource();
+
+    if (!search) {
+      this.mdbTable.setDataSource(this.previous);
+      this.elements = this.mdbTable.getDataSource();
+    }
+
+    if (search) {
+      this.elements = this.mdbTable.searchLocalDataBy(search);
+      this.mdbTable.setDataSource(prev);
+    }
+  }
 }
