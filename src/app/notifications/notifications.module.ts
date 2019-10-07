d01ba0b88f8af93443a490da464b0e228c101508
kevin lugaziva <kevin.lugaziwa@financeplan.biz>
2019-10-07 10:49:55 +0300

    HM-88 | add sms-notification component

--- src/app/notifications/notifications.module.ts
+++ src/app/notifications/notifications.module.ts
@@ -4,14 +4,15 @@ import { CommonModule } from '@angular/common';
 import { NotificationsRoutingModule } from './notifications-routing.module';
 import { NotificationsComponent } from './notifications.component';
 import { EmailNotificationComponent } from './email-notification/email-notification.component';
+import { SmsNotificationsComponent } from './sms-notifications/sms-notifications.component';
 
 
 @NgModule({
-  declarations: [NotificationsComponent, EmailNotificationComponent],
+  declarations: [NotificationsComponent, EmailNotificationComponent, SmsNotificationsComponent],
   imports: [
     CommonModule,
     NotificationsRoutingModule
   ],
-  exports: [EmailNotificationComponent]
+  exports: [EmailNotificationComponent, SmsNotificationsComponent]
 })
 export class NotificationsModule { }
---
+++ src/app/notifications/sms-notifications/sms-notifications.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sms-notifications',
+  templateUrl: './sms-notifications.component.html',
+  styleUrls: ['./sms-notifications.component.scss']
+})
+export class SmsNotificationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
