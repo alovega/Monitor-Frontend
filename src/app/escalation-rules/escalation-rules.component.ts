b7b5e99286d7919d696086c0cb49c079e0d58142
Victor Nguli <victor.joseph@financeplan.biz>
2019-10-07 05:03:45 +0300

    HM-85

---
+++ src/app/escalation-rules/escalation-rules.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hm-escalation-rules',
+  templateUrl: './escalation-rules.component.html',
+  styleUrls: ['./escalation-rules.component.scss']
+})
+export class EscalationRulesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
