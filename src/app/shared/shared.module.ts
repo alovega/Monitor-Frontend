56c23f1e54b85c279b36e95a4b8cd583756f11e8
Victor Nguli <victor.joseph@financeplan.biz>
2019-10-16 13:02:01 +0300

    HM-102 | add http-interceptor

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ChartsModule, WavesModule } from 'angular-bootstrap-md';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { LineGraphComponent } from './graphs/line-graph/line-graph.component';
 import { BarGraphComponent } from './graphs/bar-graph/bar-graph.component';
@@ -13,6 +14,7 @@ import { RadarChartComponent } from './graphs/radar-chart/radar-chart.component'
 import { DatePickerComponent } from './datetimepicker/datepicker/date.picker.component';
 import { TimePickerComponent } from './datetimepicker/timepicker/time.picker.component';
 import { EmptyDataComponent } from './empty-data/empty-data.component';
+import { HttpInterceptorService } from './helpers/http-interceptor.service';
 
 
 @NgModule({
@@ -33,7 +35,8 @@ import { EmptyDataComponent } from './empty-data/empty-data.component';
     WavesModule,
     NgbModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   exports: [
     LineGraphComponent,
@@ -45,6 +48,13 @@ import { EmptyDataComponent } from './empty-data/empty-data.component';
     DatePickerComponent,
     TimePickerComponent,
     EmptyDataComponent
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpInterceptorService,
+      multi: true
+    }
   ]
 })
 export class SharedModule { }
