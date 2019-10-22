c8bfeb58b0332c1283911dc0b30ea72400aac809
Victor Nguli <victor.joseph@financeplan.biz>
2019-10-23 02:10:29 +0300

    HM-93 | add authguard

--- src/app/shared/auth/authentication.service.ts
+++ src/app/shared/auth/authentication.service.ts
@@ -1,17 +1,40 @@
-// import { Injectable } from '@angular/core';
-// import { environment } from '../../../environments/environment';
-// import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-// import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class AuthenticationService {
-//   apiEndpoint = environment.apiEndpoint;
-//   clientId = environment.clientId;
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  apiEndpoint = environment.apiEndpoint;
+  clientId = environment.clientId;
 
-//   constructor(
-//     private http: HttpClient
-//   ) {
-//   }
-// }
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  public isAuthenticated() {
+      const userData = localStorage.getItem('user');
+      if (userData) {
+          const user = JSON.parse(userData);
+          return this.isTokenExpired(user.token, user.expiresAt);
+      }
+  }
+
+  public isTokenExpired(token, expiresIn) {
+    if ( token && expiresIn ) {
+        const now = new Date();
+        return new Date(now.getTime() + Number(expiresIn) * 1000) > now;
+    }
+  }
+
+  public login() {
+
+  }
+
+  public logout() {
+      
+  }
+}
