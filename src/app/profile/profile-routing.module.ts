ed16a409c38fdfae1b2475fc0e2562bc5e7cdb4e
kevin lugaziva <kevin.lugaziwa@financeplan.biz>
2019-10-23 16:18:55 +0300

    HM-83 | add user profile

--- src/app/profile/profile-routing.module.ts
+++ src/app/profile/profile-routing.module.ts
@@ -2,8 +2,29 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ProfileComponent } from './profile.component';
+import { DetailsComponent } from './details/details.component';
+import { NotificationComponent } from './notification/notification.component';
+import { EditComponent } from './edit/edit.component';
 
-const routes: Routes = [{ path: '', component: ProfileComponent }];
+const routes: Routes = [{ path: '', component: ProfileComponent,children:[
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'details'
+  },
+  {
+    path:'details',
+    component:DetailsComponent
+  },
+  {
+    path:'notifications',
+    component:NotificationComponent
+  },
+  {
+    path:'edit',
+    component:EditComponent
+  }
+] }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -4,14 +4,17 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
+import { NotificationComponent } from './notification/notification.component';
+import { EditComponent } from './edit/edit.component';
+import { DetailsComponent } from './details/details.component';
 
 
 @NgModule({
-  declarations: [ProfileComponent],
+  declarations: [ProfileComponent,NotificationComponent,EditComponent,DetailsComponent],
   imports: [
     CommonModule,
     ProfileRoutingModule,
-    MDBBootstrapModule
+    MDBBootstrapModule.forRoot()
   ],
   schemas: [ NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA ]
 })
