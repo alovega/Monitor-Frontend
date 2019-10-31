292b5d4a72de15c3f7f5f9e340dcdbb3e298beeb
kevin lugaziva <kevin.lugaziwa@financeplan.biz>
2019-10-31 05:51:24 +0300

    HM-111 | update profile component

--- src/app/profile/profile-routing.module.ts
+++ src/app/profile/profile-routing.module.ts
@@ -6,30 +6,32 @@ import { DetailsComponent } from './details/details.component';
 import { RecentNotificationComponent } from './recent-notification/recent-notification.component';
 import { EditComponent } from './edit/edit.component';
 import { UserNotificationsComponent } from './user-notifications/user-notifications.component';
+import { UpdatePasswordComponent } from './update-password/update-password.component';
 
-const routes: Routes = [{ path: '', component: ProfileComponent, 
-  children:[
+const routes: Routes = [{ path: '', component: ProfileComponent,
+  children: [
     {
         path: '',
         pathMatch: 'full',
         redirectTo: 'details'
       },
       {
-        path:'details',
-        component:DetailsComponent
+        path: 'details',
+        component: DetailsComponent
       },
       {
-        path:'recent-notifications',
-        component:RecentNotificationComponent
+        path: 'recent-notifications',
+        component: RecentNotificationComponent
       },
       {
-        path:'edit',
-        component:EditComponent
+        path: 'edit',
+        component: EditComponent
       }
     ]
   },
-  { path:'user-notifications', component: UserNotificationsComponent }
-]
+  { path: 'user-notifications', component: UserNotificationsComponent },
+  {path: 'update-password', component: UpdatePasswordComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
