bbe8b23a0b693dcfb1a7d3bcdd12585c685ec366
Victor Nguli <victor.joseph@financeplan.biz>
2020-01-07 16:19:30 +0300

    HM-160 | refactor public status page

--- src/app/views/public-dashboard/public-dashboard.component.ts
+++ src/app/views/public-dashboard/public-dashboard.component.ts
@@ -20,6 +20,75 @@ export class PublicDashboardComponent implements OnInit {
   systemId: string;
   endpoints: any[];
   pastDates: any;
+  public chartType: string = 'line';
+
+  public chartDatasets: Array<any> = [
+    { data: [55, 59, 50, 51, 56, 55, 40], label: 'My First dataset', lineTension: 0 },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'My Second dataset' }
+  ];
+
+  public chartDataset2: Array<any> = [
+    { data: [100, 100, 80, 100, 90, 100, 100], label: 'Total Availability', lineTension: 0 },
+  ];
+
+  public chartLabels: Array<any> = ['1', '2', '3', '4', '5', '6', '7'];
+
+  public chartColors: Array<any> = [
+    {
+      backgroundColor: 'rgba(105, 0, 132, .2)',
+      borderColor: 'rgba(200, 99, 132, .7)',
+      borderWidth: 2,
+    },
+    {
+      backgroundColor: 'rgba(0, 137, 132, .2)',
+      borderColor: 'rgba(0, 10, 130, .7)',
+      borderWidth: 2,
+    }
+  ];
+
+  public chartColors2: Array<any> = [
+    {
+      backgroundColor: 'rgb(5, 197, 5)',
+      borderColor: 'rgb(5, 172, 5)',
+      borderWidth: 2,
+    }
+  ] ;
+
+  public chartOptions: any = {
+    responsive: true,
+    legend: {
+      display: false,
+   }
+  };
+
+  public chartOptions2: any = {
+    responsive: true,
+    scales: {
+      xAxes: [{
+              display: true,
+              scaleLabel: {
+                  display: true,
+                  labelString: 'Month'
+              }
+          }],
+      yAxes: [{
+              display: true,
+              ticks: {
+                  beginAtZero: true,
+                  steps: 10,
+                  stepValue: 5,
+                  max: 100
+              }
+          }]
+    },
+    title: {
+        display: true,
+        text: 'Availability Percentage trend'
+    }
+  };
+  public chartClicked(e: any): void { }
+  public chartHovered(e: any): void { }
+
   constructor(
     private router: Router,
     private activatedRoute: ActivatedRoute,
