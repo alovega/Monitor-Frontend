7bf7037b6c87e2d51513f5dc3d1caa7f7a48a23a
kevin lugaziva <kevin.lugaziwa@financeplan.biz>
2020-10-06 09:22:13 +0300

    update environment setting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
   apiEndpoint: 'https://helamonitorapp.herokuapp.com/api',
-  clientId: '3cd49364-721a-4d3f-8bfa-141d93d6a8f7',
-  accessToken: 'ODcyZGVhMDJiYjI2YjBiZjVjNWJmNTM4MTgzYWEy'
 };
